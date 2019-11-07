a297e37aae8f72995e61f3bb47653f3290502c80
polinao <polina.olemskaia@gmail.com>
2019-11-07 11:20:21 +0100

    printing tree

--- playground.js
+++ playground.js
@@ -1,4 +1,4 @@
-
+let treeify = require('treeify');
 
 function HuffmanNode(tag,value){
     this.value = value
@@ -33,37 +33,41 @@ function lookTree(current,letter){
 }
 
 function calls(initString){
-    const arraySymbols = [new HuffmanNode('x',0)]
+    const hashTableNodes = [new HuffmanNode('x',0)]
     const enconding = []
     let output = initString[0].charCodeAt(0).toString(2) 
     
-    arraySymbols.push(new HuffmanNode(initString[0],1))
+    hashTableNodes.push(new HuffmanNode(initString[0],1))
     let huffmanTree = new HuffmanNode('x'+ initString[0],1)
-    huffmanTree.left = arraySymbols[1]
-    huffmanTree.right = arraySymbols[0]
+    huffmanTree.left = hashTableNodes[1]
+    huffmanTree.right = hashTableNodes[0]
     for(let i = 1;i<initString.length;i++){
         let isNew = false 
         const letter = initString[i]
 
-        for(let i =0;i<arraySymbols.length;i++){
-           if(arraySymbols[i].tag === letter){
-               arraySymbols[i].value=arraySymbols[i].value+1
+        for(let i =0;i<hashTableNodes.length;i++){
+           if(hashTableNodes[i].tag === letter){
+               hashTableNodes[i].value=hashTableNodes[i].value+1
                break
             }
 
-            if(i === arraySymbols.length-1){ 
+            if(i === hashTableNodes.length-1){ 
 
-                arraySymbols.push(new HuffmanNode(letter,1))
+                hashTableNodes.push(new HuffmanNode(letter,1))
                 //console.log(huffmanTree)
                 if(huffmanTree){
 
                     
                     let encondingHuffman = lookTree(huffmanTree,'x')
-                    console.log(encondingHuffman + ' encondingHuffman')
+                    //console.log(encondingHuffman + ' encondingHuffman')
                     isNew = true
                     output = output + encondingHuffman + letter.charCodeAt(0).toString(2) 
                     console.log(output + ' OUTPUT')
-                    console.log(huffmanTree)
+                    // console.log(huffmanTree)
+                    console.log(
+                        treeify.asTree(huffmanTree
+                    , true)
+                     )
                     
                 }
                 break
@@ -71,37 +75,40 @@ function calls(initString){
         }
 
         const sortable = []
-        for (let symbol in arraySymbols) {
-            sortable.push(arraySymbols[symbol])
+        for (let symbol in hashTableNodes) {
+            sortable.push(hashTableNodes[symbol])
         }
 
 
-
-
         while(sortable.length>1){
-        sortable.sort((a, b)=>a.value > b.value && a.tag.length < b.tag.length)
-        const firstTwo = sortable.splice(0,2)
+            sortable.sort((a, b)=>a.value > b.value && a.tag.length < b.tag.length)
+            const firstTwo = sortable.splice(0,2)
 
-        let temp = new HuffmanNode(firstTwo[0].tag+firstTwo[1].tag,firstTwo[0].value+firstTwo[1].value)
+            let temp = new HuffmanNode(firstTwo[0].tag+firstTwo[1].tag,firstTwo[0].value+firstTwo[1].value)
 
-        temp.left = firstTwo[1]
-        temp.right = firstTwo[0]
+            temp.left = firstTwo[1]
+            temp.right = firstTwo[0]
 
-        
-        sortable.push(temp)
+            
+            sortable.push(temp)
 
         }
         
 
             huffmanTree = sortable[0]
-            // takes a charecter 
 
             if(!isNew){
 
             output = output+lookTree(huffmanTree,letter)
-            console.log(lookTree(huffmanTree,letter))
+            //console.log(lookTree(huffmanTree,letter))
             }
-            // console.log(huffmanTree)
+            ///HUFMAN TREEE
+            //console.log('HUFMAN TREEE')
+            //  console.log(huffmanTree)
+            //  console.log(
+            //     treeify.asTree(huffmanTree
+            // , true)
+            //  )
        
     }
     return output
